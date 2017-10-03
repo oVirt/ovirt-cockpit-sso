5383ea778747b74fedb57e05cdf0d004fdf48783
Marek Libra <mlibra@redhat.com>
2017-10-03 12:45:56 +0200

    Debugging via G_MESSAGES_DEBUG

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 #!/bin/sh -
 
 echo $$ > /var/run/ovirt-cockpit-sso/ovirt-cockpit-sso.pid
+
+# Add "G_MESSAGES_DEBUG=all" bellow to turn on debug messages in /var/log/messages
 OVIRT_FQDN=$(hostname -f) XDG_CONFIG_DIRS=/usr/share/ovirt-cockpit-sso/config /usr/libexec/cockpit-ws --port=9986
